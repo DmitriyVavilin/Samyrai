229480aedd8b869f4f1a183c1df7a412f238a245
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2022-10-23 12:16:47 +0300

    reinit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className={'app-wrapper'}>
+       <header className={'header'}>
+           <img src={'https://www.logodesign.net/logo/line-art-house-roof-and-buildings-4485ld.png'}/>
+       </header>
+       <nav className={'nav'}>
+           <ul>
+               <li><a>Profile</a></li>
+               <li><a>Messages</a></li>
+               <li><a>News</a></li>
+               <li><a>Music</a></li>
+               <li><a>Settings</a></li>
+           </ul>
+       </nav>
+       <div className={'content'}>
+           <div>
+               <img src={'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'}/>
+           </div>
+           <div> avatar + description</div>
+           <div>
+               My post
+               <div>New post</div>
+               <div>
+                   post 1
+               </div>
+               <div>
+                   post 2
+               </div>
+           </div>
+       </div>
+   </div>
   );
 }
+
 
 export default App;
