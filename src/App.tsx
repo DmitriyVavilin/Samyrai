6e21e1e3caea9a5ad6faf9cc3f23ec246e3cf765
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2022-10-25 21:44:18 +0300

    six commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
 
 function App() {
-  return (
-   <div className={'app-wrapper'}>
-       <Header/>
-       <NavBar/>
-       <Profile/>
-   </div>
-  );
+    return (
+        <div className={'app-wrapper'}>
+            <Header/>
+            <NavBar/>
+            {/*<Profile/>*/}
+            <div className={'app-wrapper-content'}>
+               <Dialogs/>
+            </div>
+        </div>
+
+    );
 }
 
 
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Dialogs = () => {
+    return(
+        <div>
+            Dialogs
+        </div>
+    )
+}
