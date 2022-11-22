7ee59450aad0411802b9f1ed4d8331ea5bece0dc
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2022-11-22 12:44:58 +0300

    change state commit

--- src/components/redux/state.tsx
+++ src/components/redux/state.tsx
@@ -16,29 +16,42 @@ export type DialogsType = {
     name: string
 }
 
+export type ObjectDialogs = {
+    dialogs: DialogsType[]
+    messages: MessageType[]
+}
+
+export type ObjectPost = {
+    myPostData: PostType[]
+}
+
 export type StatePropsType = {
-    dialogs: Array<DialogsType>
-    messages: Array<MessageType>
-    myPostData?: Array<PostType>
+    profilePost: ObjectPost
+    profileDialogs: ObjectDialogs
 }
 
 export let state: StatePropsType = {
-    dialogs: [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrey'},
-        {id: 3, name: 'Sveta'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Tatyana'}
-    ],
-    messages: [
-        {id: 1, message: 'Hello!!!'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'What do you do?'},
-        {id: 4, message: 'I have a two dogs'},
-        {id: 5, message: 'I`m want you)'}
-    ],
-    myPostData: [
-        {id: 1, messages: 'Hi, how are you?', likesCount: 100},
-        {id: 2, messages: 'It`s my first post', likesCount: 150},
-    ]
+    profileDialogs: {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Tatyana'}
+        ],
+        messages: [
+            {id: 1, message: 'Hello!!!'},
+            {id: 2, message: 'How are you?'},
+            {id: 3, message: 'What do you do?'},
+            {id: 4, message: 'I have a two dogs'},
+            {id: 5, message: 'I`m want you)'}
+        ]
+    },
+    profilePost: {
+        myPostData: [
+            {id: 1, messages: 'Hi, how are you?', likesCount: 100},
+            {id: 2, messages: 'It`s my first post', likesCount: 150},
+        ]
+    }
+
 }
