3f929f9bbf31f05fefae8760c68fe28cc73ac855
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-02-06 15:24:38 +0300

    49 class

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,6 +5,7 @@ import {NavLink} from "react-router-dom";
 
 export const NavBar = () => {
     return (
+
         <nav className={s.nav}>
             <ul className={s.item}>
                 <li><NavLink to={'/profile'} activeClassName={s.active}>Profile</NavLink></li>
