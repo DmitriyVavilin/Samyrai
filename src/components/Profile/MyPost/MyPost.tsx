d6bfdd2188105b4434a3679a4c9dabe4a3f37367
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-08-01 00:38:27 +0500

    87 lesson completed

--- src/components/Profile/MyPost/MyPost.tsx
+++ src/components/Profile/MyPost/MyPost.tsx
@@ -4,16 +4,14 @@ import {Post} from "./Post/Post";
 import {MyPostPropsType} from "./MyPostContainer";
 import {AddNewPostFormRedux, AddNewPostFormType} from "./AddNewPostForm/AddNewPostForm";
 
-export const MyPost = (props: MyPostPropsType) => {
+export const MyPost = React.memo((props: MyPostPropsType) => {
 
     const postElements = props.posts.map((el, index) => <Post key={index} messages={el.messages}
                                                               likesCount={el.likesCount}/>)
 
-
     const addNewProfile = (values: AddNewPostFormType) => {
         props.addPost(values.newPost)
     }
-
     return (
         <div className={s.postBlock}>
             <h3 className={s.h3}>My post</h3>
@@ -27,4 +25,4 @@ export const MyPost = (props: MyPostPropsType) => {
             </div>
         </div>
     )
-}
+})
